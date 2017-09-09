c57a026ffda0c36267dee810179635cc242f47b1
garconde <david.jose97@hotmail.com>
2017-09-09 01:27:29 -0500

    Primer commit - inicio del proyecto

---
+++ bd/estudiantes.sql
@@ -0,0 +1,52 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 07-09-2017 a las 19:18:54
+-- Versión del servidor: 10.1.16-MariaDB
+-- Versión de PHP: 5.6.24
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `matriculas`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `estudiantes`
+--
+
+CREATE TABLE `estudiantes` (
+  `nombre` varchar(25) COLLATE utf8_spanish_ci NOT NULL,
+  `apellidos` varchar(25) COLLATE utf8_spanish_ci NOT NULL,
+  `codigo` int(11) NOT NULL,
+  `id` int(11) NOT NULL,
+  `semestre_actual` smallint(6) NOT NULL DEFAULT '0',
+  `latitud` int(11) NOT NULL,
+  `longitud` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `estudiantes`
+--
+ALTER TABLE `estudiantes`
+  ADD PRIMARY KEY (`codigo`),
+  ADD UNIQUE KEY `id` (`id`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ conectabd.php
@@ -0,0 +1,28 @@
+﻿<!-- Conexión a Bases de Datos.
+
+<?php 
+
+function conectarse() 
+{
+
+   $host = "localhost";
+   $usuario = "root";
+   $password = "";
+   $BaseDatos = "estudiante";
+ 
+   if (!($link=mysql_connect($host,$usuario,$password))) 
+   { 
+      echo "Error conectando a la base de datos."; 
+      exit(); 
+   } 
+   if (!mysql_select_db($BaseDatos,$link)) 
+   { 
+      echo "Error seleccionando la base de datos."; 
+      exit(); 
+   } 
+
+   return $link; 
+} 
+
+
+?> 
