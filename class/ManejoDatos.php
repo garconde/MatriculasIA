8c8275d993e4ac6831e9fd21391a17141b1b7ca4
jdpra <jdpra@JPRASCA-PC2>
2017-09-10 15:08:53 -0500

    2017-09-10

--- class/ManejoDatos.php
+++ class/ManejoDatos.php
@@ -34,4 +34,39 @@ class ManejoDatos {
         
     }
     
+    public function extraerListas($lista){        
+        $query = "";
+        switch ($lista){
+            case 1:
+                $query = "SELECT * FROM programas;";
+                break;
+            case 2:
+                $query = "SELECT * FROM novedades_tipo;";
+                break;
+            case 3:
+                $query = "SELECT * FROM municipios;";
+                break;
+        }        
+        $resulSet = $this->adminDB->realizar_consulta($query, $this->DB);
+        $arreglo = null;
+        $i = 0;
+        while ($row = mysqli_fetch_assoc($resulSet)){
+            switch ($lista){
+                case 1:
+                    $arreglo[$i]["id"] = $row["prog_codigo"];
+                    $arreglo[$i]["nombre"] = $row["prog_nombre"];
+                    break;
+                case 2:
+                    $arreglo[$i]["id"] = $row["nov_codigo"];
+                    $arreglo[$i]["nombre"] = $row["nov_tipo"];                    
+                    break;
+                case 3:
+                    $arreglo[$i]["id"] = $row["mun_codigo"];
+                    $arreglo[$i]["nombre"] = $row["mun_nombre"];
+                    break;
+            }
+        }        
+        return $arreglo;
+    }
+    
 }
